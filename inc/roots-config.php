6283bff00cfebb363756ccc408174ccaa60acc35
Rakesh Tembhurne <rakesh@tembhurne.com>
2012-02-25 15:56:08 +0530

    by default header and sidebars are enabled

--- inc/roots-config.php
+++ inc/roots-config.php
@@ -1,13 +1,13 @@
 <?php
 
 define('POST_EXCERPT_LENGTH',  40);
-define('BOOTSTRAP_RESPONSIVE', false);
+define('BOOTSTRAP_RESPONSIVE', true);
 define('WRAP_CLASSES',         'container');
 define('CONTAINER_CLASSES',    'row');
 define('MAIN_CLASSES',         'span8');
 define('SIDEBAR_CLASSES',      'span4');
 define('FULLWIDTH_CLASSES',    'span12');
-define('GOOGLE_ANALYTICS_ID',  'UA-4295037-2');
+define('GOOGLE_ANALYTICS_ID',  'UA-XXXXXXX-X');
 define('WIDE_HEADER',          true);
 define('LEFT_RIGHT_SIDEBARS',  true);
 
